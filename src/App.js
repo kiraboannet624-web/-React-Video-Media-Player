11c5247ce7c331e02efca64fef5fd7458cb81098
kiraboannet624 <kiraboannet624@gmail.com>
2026-03-14 21:30:54 +0200

    initial project setup with folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Navbar from "./components/Navbar";
+import Feed from "./Pages/feed";
+import VideoDetails from "./Pages/VideoDetails";
+import ChannelDetails from "./Pages/ChannelDetails";
+import SearchFeed from "./Pages/SearchFeed";
+
+const queryClient = new QueryClient();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Box sx={{ backgroundColor: "#0f0f0f", minHeight: "100vh" }}>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Feed />} />
+            <Route path='/video/:id' element={<VideoDetails />} />
+            <Route path='/channel/:id' element={<ChannelDetails />} />
+            <Route path='/search/:searchTerm' element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
