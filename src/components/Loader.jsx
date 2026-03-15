e14a5e5a7e094d19f5231ed7ebf5d6555203586b
kiraboannet624 <kiraboannet624@gmail.com>
2026-03-15 18:37:50 +0200

    setup axios instance with RapidAPI configuration

--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import { Box, CircularProgress } from '@mui/material';
-
-const Loader = () => (
-  <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
-    <CircularProgress color="error" />
-  </Box>
-);
-
-export default Loader;
